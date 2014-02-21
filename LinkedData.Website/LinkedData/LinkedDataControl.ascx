d5621b6c937177117cee123fd2f53a8b107c0266
ajs <adam.simmonds@eduserv.org.uk>
2014-02-21 18:41:17 +0000

    added rdf hello world example

--- LinkedData.Website/LinkedData/LinkedDataControl.ascx
+++ LinkedData.Website/LinkedData/LinkedDataControl.ascx
@@ -1,2 +1,3 @@
 ﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="LinkedDataControl.ascx.cs" Inherits="LinkedData.Website.LinkedData.LinkedDataControl" %>
 <h2>Linked data here</h2>
+<asp:Literal runat="server" ID="litRdf"></asp:Literal>
--- LinkedData.Website/LinkedData/LinkedDataControl.ascx.cs
+++ LinkedData.Website/LinkedData/LinkedDataControl.ascx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using VDS.RDF;
+using VDS.RDF.Writing;
 
 namespace LinkedData.Website.LinkedData
 {
@@ -11,7 +13,34 @@ namespace LinkedData.Website.LinkedData
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            //Fill in the code shown on this page here to build your hello world application
+            Graph g = new Graph();
 
+            IUriNode dotNetRDF = g.CreateUriNode(UriFactory.Create("http://www.dotnetrdf.org"));
+            IUriNode says = g.CreateUriNode(UriFactory.Create("http://example.org/says"));
+            ILiteralNode helloWorld = g.CreateLiteralNode("Hello World");
+            ILiteralNode bonjourMonde = g.CreateLiteralNode("Bonjour tout le Monde", "fr");
+
+            g.Assert(new Triple(dotNetRDF, says, helloWorld));
+            g.Assert(new Triple(dotNetRDF, says, bonjourMonde));
+
+            foreach (Triple t in g.Triples)
+            {
+                Console.WriteLine(t.ToString());
+            }
+
+            NTriplesWriter ntwriter = new NTriplesWriter();
+            //ntwriter.Save(g, "HelloWorld.nt");
+
+            RdfXmlWriter rdfxmlwriter = new RdfXmlWriter();
+            //rdfxmlwriter.Save(g, "HelloWorld.rdf");
+
+            System.IO.StringWriter sw = new System.IO.StringWriter();
+
+            //Call the Save() method to write to the StringWriter
+            rdfxmlwriter.Save(g, sw);
+
+            litRdf.Text = HttpUtility.HtmlEncode(sw.ToString());
         }
     }
 }

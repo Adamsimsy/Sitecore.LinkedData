a64914b9a54895b1d6e8b2ea280b859aa963dfb5
adamsimsy <adamsimsy@gmail.com>
2014-12-30 22:37:15 +0000

    Revert "Fixed formatter which changes Uri to Url which was creating subject as a literal which made it difficult to do do related queries such as the following:"
    SELECT ?p2 ?o2 WHERE { <http://sitecorelinkeddata/sitecore/shell/Leagues/2014/07/01/17/12/La%20Liga> ?p ?o . ?o ?p2 ?o2 .}

--- LinkedData/Formatters/UriToDynamicUrlFormatter.cs
+++ LinkedData/Formatters/UriToDynamicUrlFormatter.cs
@@ -30,7 +30,7 @@ namespace LinkedData.Formatters
 
             IUriNode sub = g.CreateUriNode(new Uri(subjectUrl));
             IUriNode pred = g.CreateUriNode(new Uri(triple.Predicate.ToString().Split('#')[0]));
-            IUriNode obj = g.CreateUriNode(new Uri(objectUrl));
+            ILiteralNode obj = g.CreateLiteralNode(objectUrl);
 
             triple = new Triple(sub, pred, obj);
 

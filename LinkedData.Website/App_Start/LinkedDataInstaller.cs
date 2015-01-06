919d67af9acc733d91631e1a34e77444ffc89185
adamsimsy <adamsimsy@gmail.com>
2015-01-06 23:07:04 +0000

    Added vanilla config for the installer

--- LinkedData.Website/App_Start/LinkedDataInstaller.cs
+++ LinkedData.Website/App_Start/LinkedDataInstaller.cs
@@ -23,10 +23,10 @@ namespace LinkedData.Website.App_Start
         {
             var container = new WindsorContainer();
 
-            container.Register(Component.For<IQueryableStorage>().ImplementedBy<SesameHttpProtocolVersion6Connector>().LifestyleSingleton()
-                .DependsOn(Dependency.OnValue("baseUri", "http://54.154.29.33:8080/openrdf-sesame"), Dependency.OnValue("storeID", "in-mem-sesame")));
+            //container.Register(Component.For<IQueryableStorage>().ImplementedBy<SesameHttpProtocolVersion6Connector>().LifestyleSingleton()
+                //.DependsOn(Dependency.OnValue("baseUri", "http://server_name:8080/openrdf-sesame"), Dependency.OnValue("storeID", "repository_name")));
 
-           // container.Register(Component.For<IQueryableStorage>().ImplementedBy<InMemoryManager>().LifestyleSingleton());
+            container.Register(Component.For<IQueryableStorage>().ImplementedBy<InMemoryManager>().LifestyleSingleton());
 
             var concepts = new List<BaseConcept>();
 

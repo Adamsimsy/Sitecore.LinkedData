2243ea9efec4ba0a8d0577da2848d1f51a542cee
adamsimsy <adamsimsy@gmail.com>
2015-01-10 16:02:16 +0000

    Fixed all references to SitecoreLinkedDataInstaller

--- LinkedData.Website/App_Start/SitecoreLinkedDataInstaller.cs
+++ LinkedData.Website/App_Start/SitecoreLinkedDataInstaller.cs
@@ -23,17 +23,17 @@ namespace LinkedData.Website.App_Start
         {
             var container = new WindsorContainer();
 
-            //container.Register(Component.For<IQueryableStorage>().ImplementedBy<SesameHttpProtocolVersion6Connector>().LifestyleSingleton()
-            //.DependsOn(Dependency.OnValue("baseUri", "http://server_name:8080/openrdf-sesame"), Dependency.OnValue("storeID", "repository_name")));
+            container.Register(Component.For<IQueryableStorage>().ImplementedBy<SesameHttpProtocolVersion6Connector>().LifestyleSingleton()
+                .DependsOn(Dependency.OnValue("baseUri", "http://54.154.29.33:8080/openrdf-sesame"), Dependency.OnValue("storeID", "in-mem-sesame")));
 
-            container.Register(Component.For<IQueryableStorage>().ImplementedBy<InMemoryManager>().LifestyleSingleton());
+            //container.Register(Component.For<IQueryableStorage>().ImplementedBy<InMemoryManager>().LifestyleSingleton());
 
             var concepts = new List<BaseConcept>();
 
             concepts.Add(new SitecoreTemplateConcept() { SubjectTemplateName = "league", ConceptUri = new Uri("http://football.com/league-to-team"), ObjectTemplateName = "team" });
             concepts.Add(new SitecoreTemplateConcept() { SubjectTemplateName = "team", ConceptUri = new Uri("http://football.com/team-to-player"), ObjectTemplateName = "player" });
             concepts.Add(new SitecoreTemplateConcept() { SubjectTemplateName = "newsstory", ConceptUri = new Uri("http://football.com/news-to-item"), ObjectTemplateName = "*" });
-   
+
             container.Register(Component.For<IConceptProvider>().Instance(new SitecoreConceptProvider(concepts)).LifestyleSingleton());
 
             container.Install(new LinkedDataInstallerSitecore());

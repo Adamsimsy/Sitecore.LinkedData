a72ed5fab87d35b10c169e9fac0ac9db1f37c346
adamsimsy <adamsimsy@gmail.com>
2015-01-11 00:21:19 +0000

    Added build for version 0.1.8

--- LinkedData/Properties/AssemblyInfo.cs
+++ LinkedData/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Sitecore.LinkedData")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Sitecore Linked Data Module")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Adamsimsy")]
 [assembly: AssemblyProduct("Sitecore.LinkedData")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.8.0")]
+[assembly: AssemblyFileVersion("0.1.8.0")]
